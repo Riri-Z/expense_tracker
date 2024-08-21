02ac3da6331aa36446af8bd9905176e7d68e38c6
riri z <riri.zamor@gmail.com>
2024-08-22 00:12:44 +0200

    add getters/setters

--- src/main/java/com/expense_tracker/user/UserInfo.java
+++ src/main/java/com/expense_tracker/user/UserInfo.java
@@ -28,4 +28,48 @@ public class UserInfo {
 
 	private String roles;
 
+	public Long getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRoles() {
+		return this.roles;
+	}
+
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+
 }
