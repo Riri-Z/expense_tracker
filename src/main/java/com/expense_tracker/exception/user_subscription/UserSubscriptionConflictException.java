75fa6085070baf9cc91fd301774eeb2cebd2a31c
Riri-Z <you@example.com>
2024-08-30 11:41:36 +0200

    Feat : Add exception handler

---
+++ src/main/java/com/expense_tracker/exception/user_subscription/UserSubscriptionConflictException.java
@@ -0,0 +1,10 @@
+package com.expense_tracker.exception.user_subscription;
+
+public class UserSubscriptionConflictException extends RuntimeException {
+
+	public UserSubscriptionConflictException(String message) {
+		super(message);
+
+	}
+
+}
