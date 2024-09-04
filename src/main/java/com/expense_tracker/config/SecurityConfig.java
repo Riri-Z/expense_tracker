810deade68448a92e414a72570c062ac9f5adbe6
Riri-Z <riri.zamor@gmail.com>
2024-09-04 18:38:32 +0200

    Format

--- src/main/java/com/expense_tracker/config/SecurityConfig.java
+++ src/main/java/com/expense_tracker/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter authFilter,
 			AuthenticationProvider authenticationProvider) throws Exception {
-				
+
 		return http.csrf(csrf -> csrf.disable()) // disable for stateless apis
 			.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
 				.requestMatchers("/auth/welcome", "/auth/addNewUser", "/auth/generateToken", "/request-password-reset",
